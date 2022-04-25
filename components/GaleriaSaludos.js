6ddc27c40ebb8c77c1d542b5881a9d7e462d991a
Hanhusaso <hans.e.huiza.n@gmail.com>
2022-04-25 16:04:10 -0500

    primer commit test

---
+++ components/GaleriaSaludos.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const GaleriaSaludos = () => {
+	return <div>GaleriaSaludos</div>;
+};
---
+++ components/Interactivo.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Interactivo = () => {
+	return <div>Interactivo</div>;
+};
