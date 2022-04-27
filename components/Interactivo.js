87e6b37fbd5eee3566a390cdb63bdff1eb05c79b
Hanhusaso <hans.e.huiza.n@gmail.com>
2022-04-27 10:05:28 -0500

    imgs ubicadas

--- components/Interactivo.js
+++ components/Interactivo.js
@@ -1,5 +1,75 @@
 import React from 'react';
 
 export const Interactivo = () => {
-	return <div>Interactivo</div>;
+	return (
+		<>
+			<section className="interactivo min-h-screen relative">
+				<div className="info-page absolute top-14 left-1/2 -translate-x-1/2">
+					<p>logo unmsm</p>
+					<p>
+						<span>471.°</span>
+						<br />
+						<span>Aniversario</span>
+					</p>
+					<p>¡San Marcos es el Perú!</p>
+					<p>12 de mayo</p>
+					<p>1551-2022</p>
+				</div>
+				<div className="flex pt-14">
+					<div>
+						<img src="/img/personajes/Carrion-edit.png" alt="" />
+					</div>
+					<div className="relative top-20">
+						<img
+							src="/img/personajes/Rosa-Alarco-compl.png"
+							alt=""
+						/>
+					</div>
+					<div className="relative top-40">
+						<img
+							src="/img/personajes/Jorge-Basadre-complete.png"
+							alt=""
+						/>
+					</div>
+					<div className="relative top-60">
+						<img
+							src="/img/personajes/Ella-Dumbar-compl.png"
+							alt=""
+						/>
+					</div>
+					<div className="relative top-80">
+						<img
+							src="/img/personajes/VargasLlosa-compl.png"
+							alt=""
+						/>
+					</div>
+					<div className="relative top-80">
+						<img
+							src="/img/personajes/Ruth-Shady-copmle.png"
+							alt=""
+						/>
+					</div>
+					<div className="relative top-60">
+						<img
+							src="/img/personajes/Raul-Porras-compl.png"
+							alt=""
+						/>
+					</div>
+					<div className="relative top-40">
+						<img src="/img/personajes/Maria Luisa_.png" alt="" />
+					</div>
+					<div className="relative top-20">
+						<img
+							src="/img/personajes/Julio-C-completo.png"
+							alt=""
+						/>
+					</div>
+					<div>
+						<img src="/img/personajes/mayolo-completo.png" alt="" />
+					</div>
+				</div>
+				<div></div>
+			</section>
+		</>
+	);
 };
