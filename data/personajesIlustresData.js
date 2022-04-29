34d431ac3f91192e45efd67302198f5665f22882
Luis Castillo <lcastillov123@gmail.com>
2022-04-29 13:45:32 -0500

    data demintroduccion de personajes ilustres

--- data/personajesIlustresData.js
+++ data/personajesIlustresData.js
@@ -9,7 +9,8 @@ const personajesIlustresData = [
 		slug: '',
 		imagenGrande:
 			'https://biologia-unmsm.s3.us-east-2.amazonaws.com/carrion_grande_e0d3747480.png?updated_at=2022-04-29T15:57:00.123Z',
-		introduccion: 'asdadasdasd qeqwsdc sdcf',
+		introduccion:
+			'Nacida en Lima, el 10 de junio de 1918, la destacada historiadora y abogada fue la primera en ocupar una cátedra universitaria en el Perú, además de inaugurar la participación de la mujer en la Academia Nacional de Historia, en la junta directiva del Colegio de Abogados de Lima y en la Corte Superior de Lima.',
 		seleccionado: false,
 		videoSaludo:
 			'https://afroperuanos.s3.us-east-2.amazonaws.com/videos/Martin+Alvarado-1.m4v',
@@ -24,7 +25,8 @@ const personajesIlustresData = [
 			'https://biologia-unmsm.s3.us-east-2.amazonaws.com/rosa_alarco_grande_205fa46785.png?updated_at=2022-04-29T15:57:01.910Z',
 		desplazamiento: '46px',
 		slug: '',
-		introduccion: ' ois sdfogji ',
+		introduccion:
+			'Nacida en noviembre de 1911, fue pionera en la fundación de coros juveniles y como musicóloga dedicó su vida a estudiar el folclore y melodías antiguas de la costa y de la sierra; perfeccionando sus conocimientos de música en conservatorios de Chile, Francia y España.',
 		seleccionado: false,
 		videoSaludo: '',
 	},
@@ -53,7 +55,8 @@ const personajesIlustresData = [
 		slug: '',
 		imagenGrande:
 			'https://biologia-unmsm.s3.us-east-2.amazonaws.com/ella_dumbar_grande_86eeaa1cdb.png?updated_at=2022-04-29T15:57:00.083Z',
-		introduccion: 'qdvqdvas',
+		introduccion:
+			'Nacida en Lima, el 10 de junio de 1918, la destacada historiadora y abogada fue la primera en ocupar una cátedra universitaria en el Perú, además de inaugurar la participación de la mujer en la Academia Nacional de Historia, en la junta directiva del Colegio de Abogados de Lima y en la Corte Superior de Lima.',
 		seleccionado: false,
 		videoSaludo:
 			'https://afroperuanos.s3.us-east-2.amazonaws.com/videos/Martin+Alvarado-1.m4v',
@@ -68,7 +71,8 @@ const personajesIlustresData = [
 		slug: '',
 		imagenGrande:
 			'https://biologia-unmsm.s3.us-east-2.amazonaws.com/jorge_basadre_grande_43b53e1704.png?updated_at=2022-04-29T15:57:00.341Z',
-		introduccion: 'jkiyl  fghfdsgh fg ',
+		introduccion:
+			'Autor de la Historia de la República del Perú, considerada la obra más relevante de la historiografía peruana del siglo XX, el profesor emérito de la UNMSM nació en Tacna, el 12 de febrero de 1903, época en que dicha ciudad, al igual que Arica, estaba desde 1880 bajo administración chilena.',
 		seleccionado: false,
 		videoSaludo: '',
 	},
@@ -82,7 +86,8 @@ const personajesIlustresData = [
 		slug: '',
 		imagenGrande:
 			'https://biologia-unmsm.s3.us-east-2.amazonaws.com/maria_luisa_grande_14b667afe5.png?updated_at=2022-04-29T15:57:00.646Z',
-		introduccion: 'sd fgsrgsdfg',
+		introduccion:
+			'Nacida en la ciudad de Jauja, el 20 de junio de 1938, la doctora Aguilar ingresó en 1958 a la Escuela de Matemáticas de la UNMSM, para luego, viajar a la Argentina a estudiar la carrera de Astronomía en el Observatorio de Astronomía y Geofísica de la Universidad Nacional de La Plata, debido a que en el Perú no existía esa especialidad profesional, especializándose en espectroscopia estelar, atmósferas estelares y estrellas variables.',
 		seleccionado: false,
 		videoSaludo:
 			'https://afroperuanos.s3.us-east-2.amazonaws.com/videos/Martin+Alvarado-1.m4v',
@@ -97,7 +102,8 @@ const personajesIlustresData = [
 		slug: '',
 		imagenGrande:
 			'https://biologia-unmsm.s3.us-east-2.amazonaws.com/ruthsahdy_grande_c9156bfe17.png?updated_at=2022-04-29T15:57:01.802Z',
-		introduccion: 'ra gsdf ',
+		introduccion:
+			'Es una reconocida arqueóloga e investigadora por su labor de revalorización y datación de sitio arqueológico de la Ciudad Sagrada de Caral, la más antigua de América, y por su larga trayectoria profesional dirigiendo distintos proyectos de investigación arqueológica en la costa, sierra y selva del Perú que han dado énfasis al estudio del desarrollo de las organizaciones sociopolíticas complejas.',
 		seleccionado: false,
 		videoSaludo: '',
 	},
@@ -111,7 +117,8 @@ const personajesIlustresData = [
 		slug: '',
 		imagenGrande:
 			'https://biologia-unmsm.s3.us-east-2.amazonaws.com/raul_porras_grande_9f3b806b5e.png?updated_at=2022-04-29T15:57:01.644Z',
-		introduccion: 'wert werg',
+		introduccion:
+			'Diplomático, historiador, catedrático, abogado y ensayista peruano, nació en la ciudad de Pisco, el 23 de marzo de 1897, siendo memorable su actuación principista en la reunión de cancilleres de la Organización de Estados Americanos donde rechaza el bloqueo a Cuba. Su residencia Miraflores es, actualmente, casa -museo y sede del instituto que lleva su nombre.',
 		seleccionado: false,
 		videoSaludo: '',
 	},
@@ -125,7 +132,8 @@ const personajesIlustresData = [
 		slug: '',
 		imagenGrande:
 			'https://biologia-unmsm.s3.us-east-2.amazonaws.com/julo_c_tello_grande_dc893ba5a0.png?updated_at=2022-04-29T15:57:00.421Z',
-		introduccion: 'wer gefg  ds',
+		introduccion:
+			'Nacido el 11 de abril de 1880, en Huarochirí, el destacado médico cirujano, arqueólogo y antropólogo es considerado el padre de la arqueología peruana, descubridor de las culturas Chavín y Paracas y fundador del Museo de Arqueología, Antropología e Historia del Perú.',
 		seleccionado: false,
 		videoSaludo: '',
 	},
@@ -139,7 +147,8 @@ const personajesIlustresData = [
 		slug: '',
 		imagenGrande:
 			'https://biologia-unmsm.s3.us-east-2.amazonaws.com/santiago_atunez_grande_2d20868a88.png?updated_at=2022-04-29T15:57:02.162Z',
-		introduccion: 'sdfg sfvs dt',
+		introduccion:
+			'Candidato al Premio Nobel de Física en 1943, el físico, ingeniero y matemático nació el 10 de enero de 1887, en Áncash; se graduó como doctor en Ciencias Matemáticas con la tesis sobre la “Teoría Cinética del Potencial Newtoniano y algunas aplicaciones a las Ciencias Físicas”, y en Francia se tituló de Ingeniero Electricista en la Universidad de Grenoble.',
 		seleccionado: false,
 		videoSaludo: '',
 	},
