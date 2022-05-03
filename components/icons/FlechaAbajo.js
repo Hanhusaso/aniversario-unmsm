10a72e3ae47c26d2d8c9203d49d71bb2fe4c0a65
Hanhusaso <hans.e.huiza.n@gmail.com>
2022-05-03 12:02:37 -0500

    custom select en personajes ilustres

--- components/icons/FlechaAbajo.js
+++ components/icons/FlechaAbajo.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const FlechaAbajo = () => {
+const FlechaAbajo = ({ animate, rojo }) => {
 	return (
 		<svg
-			className="mt-2 animate-bounce"
+			className={`mt-2 ${animate ? 'animate-bounce' : ''}`}
 			width="20"
 			height="21"
 			viewBox="0 0 20 21"
@@ -11,7 +11,7 @@ const FlechaAbajo = () => {
 			xmlns="http://www.w3.org/2000/svg">
 			<path
 				d="M3 7.50708L10 13.5071L17 7.50707"
-				stroke="#591614"
+				stroke={`${rojo ? '#591614' : '#191D29'}`}
 				strokeWidth="3"
 				strokeLinecap="round"
 				strokeLinejoin="round"
