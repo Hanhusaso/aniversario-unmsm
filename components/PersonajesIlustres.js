3dea5e7bb0076942ab86938b7d8a62a105e9cdd1
Hanhusaso <hans.e.huiza.n@gmail.com>
2022-05-03 10:33:47 -0500

    select personajes ilustres

--- components/PersonajesIlustres.js
+++ components/PersonajesIlustres.js
@@ -7,8 +7,6 @@ import { Modal } from './Modal';
 import ReactPlayer from 'react-player';
 import PersonajesContext from '../contexts/personajesContext';
 
-import { useWindowSize } from '../utils/useWindowSize';
-
 export const PersonajesIlustres = () => {
 	const {
 		setPersonajesIlustresRef,
@@ -17,8 +15,6 @@ export const PersonajesIlustres = () => {
 		personajeElegido,
 		setPersonajeElegido,
 	} = useContext(PersonajesContext);
-
-	const { width, height } = useWindowSize();
 
 	const [estadoModal, setEstadoModal] = useState(false);
 	const [modalData, setModalData] = useState(null);
@@ -41,26 +37,28 @@ export const PersonajesIlustres = () => {
 			<div
 				className="bg-gris"
 				id="personajesIlustres"
-				ref={personajesIlustresRef}
-			>
-				<div className="container mx-auto px-24">
-					<div className="pt-16 flex items-start">
-						<h1 className="text-amarillo font-adelleBold font-semibold text-5xl mr-16 leading-none -translate-y-2">
-							Personajes ilustres
-						</h1>
-						<p className="pb-16">
-							La figura de hombres y mujeres que dejaron su legado
-							a futuras generaciones en los diversos campos del
-							conocimiento, engalana el afiche del 471°
-							aniversario de la Universidad Nacional Mayor de San
-							Marcos (UNMSM) que conmemorará el próximo 12 de
-							mayo. A continuación, una breve reseña que destaca
-							los aportes de un grupo de ilustres sanmarquinos.
-						</p>
+				ref={personajesIlustresRef}>
+				<div className="container mx-auto">
+					<div className="mx-4 lg:mx-24">
+						<div className="pt-11 lg:pt-16 flex flex-col lg:flex-row">
+							<h1 className="text-amarillo mb-2 lg:mb-0 font-adelleBold font-semibold text-4xl md:text-5xl mr-16 leading-none translate-y-0 lg:-translate-y-2">
+								Personajes ilustres
+							</h1>
+							<p className="pb-8 md:pb-16">
+								La figura de hombres y mujeres que dejaron su
+								legado a futuras generaciones en los diversos
+								campos del conocimiento, engalana el afiche del
+								471° aniversario de la Universidad Nacional
+								Mayor de San Marcos (UNMSM) que conmemorará el
+								próximo 12 de mayo. A continuación, una breve
+								reseña que destaca los aportes de un grupo de
+								ilustres sanmarquinos.
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
-			<div className="bg-gris">
+			<div className="bg-gris hidden md:block">
 				<div className="container mx-auto relative">
 					<div className="grid grid-cols-10">
 						{personajes.map((pers, index) => (
@@ -84,8 +82,7 @@ export const PersonajesIlustres = () => {
 									personajes[index].seleccionado &&
 									'opacity-50'
 								} relative cursor-pointer col-span-1 hover:opacity-50`}
-								style={{ left: pers.desplazamiento }}
-							>
+								style={{ left: pers.desplazamiento }}>
 								<Image
 									src={pers.imagenMiniatura}
 									width={230}
@@ -98,27 +95,44 @@ export const PersonajesIlustres = () => {
 					</div>
 				</div>
 			</div>
-			<div className="bg-personajesIlustres bg-no-repeat bg-cover bg-azulDegradado">
-				<div className="pt-9 container mx-auto flex flex-row justify-center">
-					<div className="w-[29%]">
-						<Image
-							src={personajeElegido.imagenGrande}
-							width={431}
-							height={480}
-							layout="responsive"
-							draggable={false}
-							quality={100}
-							className="brightness-125"
-						/>
+			<div className="px-4 md:px-0 pt-8 md:pt-0 bg-personajesIlustres bg-no-repeat bg-cover bg-azulDegradado">
+				<div className="block md:hidden">
+					<select
+						className="w-full h-14 px-5 py-4 font-lato font-bold"
+						defaultValue={2}
+						onChange={(e) => {
+							setPersonajeElegido(personajes[e.target.value]);
+							console.log(e.target.value);
+						}}>
+						{personajes.map((pers, index) => (
+							<option key={pers.nombres} value={index}>
+								{pers.nombres + ' ' + pers.apellidos}
+							</option>
+						))}
+					</select>
+				</div>
+				<div className="pt-9 container mx-auto flex flex-col md:flex-row justify-center">
+					<div className="w-full md:w-[29%]">
+						<div className="mx-auto w-1/2 md:w-full">
+							<Image
+								src={personajeElegido.imagenGrande}
+								width={431}
+								height={480}
+								layout="responsive"
+								draggable={false}
+								quality={100}
+								className="brightness-125"
+							/>
+						</div>
 					</div>
-					<div className="w-[42%] ml-8 text-blanco">
-						<h1 className="pt-10 text-[1.75rem]">
+					<div className="w-full md:w-[42%] ml-0 md:ml-8 text-blanco">
+						<h1 className="hidden md:block pt-10 text-[1.75rem]">
 							{personajeElegido.nombres}
 						</h1>
-						<h1 className="text-4xl">
+						<h1 className="hidden md:block text-4xl">
 							{personajeElegido.apellidos}
 						</h1>
-						<p className="mt-9">
+						<p className="mt-9 mb-9">
 							{personajeElegido.introduccion}
 							<Link href={'/' + personajeElegido.slug}>
 								<a>
@@ -136,8 +150,7 @@ export const PersonajesIlustres = () => {
 									);
 									setEstadoModal(true);
 								}}
-								className="flex justify-center items-center bg-rojoclaro py-2 px-3 rounded-lg mt-5"
-							>
+								className="flex justify-center items-center bg-rojoclaro py-2 px-3 rounded-lg mt-5">
 								<Triangulo className="mr-3" /> Ver saludo
 							</button>
 						)}
