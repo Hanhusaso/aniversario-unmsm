53f813d149a5f9bbc8ea3014495d7454c3f0c683
Hanhusaso <hans.e.huiza.n@gmail.com>
2022-05-05 09:33:39 -0500

    componente video descomentado e importado

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { Interactivo } from '../components/Interactivo';
 import { Programa } from '../components/Programa';
 import { GaleriaSaludos } from '../components/GaleriaSaludos';
 import { PersonajesIlustres } from '../components/PersonajesIlustres';
+import { Videos } from '../components/Videos';
 import FooterAniversario from '../components/FooterAniversario';
 
 export default function Home() {
