31e76cdd3293a098a1f5c135beca71f2d7fb75ef
Hanhusaso <hans.e.huiza.n@gmail.com>
2022-05-05 09:36:05 -0500

    corregido propiedades de iframe

--- components/Videos.js
+++ components/Videos.js
@@ -104,9 +104,9 @@ export const Videos = () => {
 								height="480"
 								src={modalData}
 								title="YouTube video player"
-								frameborder="0"
+								frameBorder="0"
 								allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-								allowfullscreen></iframe>
+								allowFullScreen></iframe>
 						) : (
 							<></>
 						))}
